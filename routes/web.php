c11a90dea412491b3f5d06a1c7dc0b1b8c5afeb5
k-ardliyan <k.ardliyan@gmail.com>
2024-04-23 23:10:36 +0700

    Add chirp functionality - Created chirps.blade.php, - Chirp.php model, - ChirpController, - and chirps route.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::get('chirps', [Controllers\ChirpController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('chirps');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
@@ -12,4 +17,4 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
